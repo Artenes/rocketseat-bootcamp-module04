caf8dcf56d60c47d33f876817c27d735ac79edd1
Artenes Nogueira <artenesama@gmail.com>
2019-09-11 20:08:22 -0400

    chore: Removes anime image from app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import anime from './assets/anime.jpg';
 import TechList from './components/TechList';
 
 function App() {
   return (<div>
-    <img src={anime} alt="Anime" width="300" height="200"/>
     <TechList />
   </div>);
 }
