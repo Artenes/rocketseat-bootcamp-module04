918669908467e9cb72b87d80992c64cbe75489c4
Artenes Nogueira <artenesama@gmail.com>
2019-09-11 20:23:42 -0400

    chore: Moves tech item to a component

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TechItem from './TechItem';
 
 class TechList extends Component {
   state = {
@@ -36,12 +37,12 @@ class TechList extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          { this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button type="button" onClick={() => {this.handleDelete(tech)}}>Delete</button>
-            </li>
-          )) }
+          { this.state.techs.map(tech => 
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)}
+            />) }
         </ul>
         <input 
           type="text" 
